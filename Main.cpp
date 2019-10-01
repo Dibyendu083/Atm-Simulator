a1d4543797aba80501cd2046582ca12db597657c
Dibyendu Chattoraj <dibyenduchattoraj830@gmail.com>
2019-10-02 01:38:23 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include<string>
-#include<fstream>
+#include<fshtream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 class Main{
