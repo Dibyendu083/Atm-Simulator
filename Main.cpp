e5871f8a09c97de41bf76e5dc72cf100ca8e1be2
Dibyendu Chattoraj <dibyenduchattoraj830@gmail.com>
2019-10-02 01:38:47 +0530

    Header file added

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include<string>
-#include<fshtream>
+#include<fstream>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 class Main{
