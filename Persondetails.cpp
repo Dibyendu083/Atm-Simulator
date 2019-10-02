3bce06de954d8ba86fcd34fc613b9643ef50c2b9
Abhi-tech13 <53057468+Abhi-tech13@users.noreply.github.com>
2019-10-02 12:42:55 +0530

    Account type class deleted and added to person

--- Persondetails.cpp
+++ Persondetails.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include "String.h"
 using namespace std;
 class PersonDetails{
 	String name;
@@ -7,9 +8,17 @@ class PersonDetails{
 	String gender;
 	String address;
 	String contactNo;
+	String cardNumber;
+	String pin;
+	String AccountType;
+	double balance=0;
+	double overdraftLimit=0;
 	public :
 		PersonDetails();
+		long int generateCardNumber();
+		int generatePin();
 };
+
 PersonDetails::PersonDetails(){
 	cout<<"constructor for person details called"<<endl;
 	cout<<"Enter Aplicant's Name (first name middle name last name):";
@@ -24,6 +33,16 @@ PersonDetails::PersonDetails(){
 	cin>>address;
 	cout<<"Enter Contact Number:";
 	cin>>contactNo;
+	cout<<"Enter Account Type:"<<endl<<"Enter 1 for Savings Account"<<endl<<"Enter 2 for Current Account"<<endl<<"Enter choice:";
+	int c;
+	cin>>c;
+	cout<<"Enter Account Opening Balance(above 1000) :";
+	cin>>balance;
+	if(c==2){
+		cout<<"Your Overdraft Limit is:"<<5*balance;
+	}
+	this->cardNumber=generateCardNumber();
+	this->pin=generatePin();
 }
 class AccountType{
 	String type;
@@ -31,10 +50,3 @@ class AccountType{
 	public:
 		AccountType(PersonDetails );
 };
-AccountType::AccountType(PersonDetails p){
-	cout<<"constructor for account type called"<<endl;
-	cout<<"Enter 1 for Savings Account"<<endl<<"Enter 2 for Current Account"<<endl<<"Enter your choice:";
-	int ch;
-	cin>>ch;
-	
-}
